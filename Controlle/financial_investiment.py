c5c30e0993a0a9d772cb5d9479980d8420bdcf0c
Vinícius Silva Queiroz <coder.fullstack.vinicius@gmail.com>
2024-05-19 11:25:25 -0300

    Correction of some bugs and improvements.

--- Controlle/financial_investiment.py
+++ Controlle/financial_investiment.py
@@ -4,8 +4,8 @@ class Financial_Investiment():
     choice_user = int(input("\nDeseja retornar a tela incial? (0 - Retornar | 1 - Sair do Sistema) "))
     match choice_user:
         case 0:
-            from Controlle.main import choices
-            choices()
+            from main import choices
+            main.choices()
         case 1:
             print("\nA Santander agradeçe o sua confiança. Até logo.")
             sys.exit()
